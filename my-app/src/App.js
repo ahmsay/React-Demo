4e4455317aaf6d50b263af06041afdeb9c932ae1
ahmsay <ahmsay@gmail.com>
2020-03-30 21:22:46 +0300

    outputting lists

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import Ninjas from './Ninjas';
 
 class App extends Component {
+  state = {
+    ninjas : [
+      { id: 1, name: 'Ryu', age: 30, belt: 'black' },
+      { id: 2, name: 'Yoshi', age: 20, belt: 'green' },
+      { id: 3, name: 'Crystal', age: 25, belt: 'pink' }
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <h1>asdzxc</h1>
-        <Ninjas name="Ryuk" age="2535435" belt="Black"/>
-        <br/>
-        <Ninjas name="Ryu" age="2" belt="White"/>
+        <h1>Ninja List</h1>
+        <Ninjas ninjas={ this.state.ninjas }/>
       </div>
     );
   }
